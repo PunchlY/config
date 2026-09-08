6de336fa1c5cb62c59f8d598cca0cd4326c5fbf9
PunchlY <punchly9lin@gmail.com>
2026-09-08 15:24:04 +0800

    chore: 更新 flake.lock 并移除 wiliwili legacyPackage

--- packages/default.nix
+++ packages/default.nix
@@ -90,17 +90,6 @@
 
       wayllpaper = final.callPackage ./wayllpaper.nix {};
 
-      wiliwili = pkgs.wiliwili.overrideAttrs {
-        version = "1.6.0";
-        src = final.fetchFromGitHub {
-          owner = "xfangfang";
-          repo = "wiliwili";
-          tag = "v1.6.0";
-          fetchSubmodules = true;
-          hash = "sha256-J6oUMUzfogsIBj1GpwWmKhjphTV628rG+3w28Dc81Fw=";
-        };
-      };
-
       wleird = final.callPackage ./wleird.nix {};
     };
 
